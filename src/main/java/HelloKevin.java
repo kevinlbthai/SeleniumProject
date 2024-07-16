da3fbc4a3126489c606a4ab303d6353cae671366
Kevin Lim <kevinlbthai79@gmail.com>
2024-07-16 15:55:57 +0800

    update the selenium libraries to verion 4.22.0

--- src/main/java/HelloKevin.java
+++ src/main/java/HelloKevin.java
@@ -1,5 +1,16 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class HelloKevin {
     public static void main(String[] args) {
         System.out.println("Hello Kevin, welcome to Selenium Project Self Learning.");
+
+        System.getProperty("webdriver.chrome.driver", "D:\\External WebDriver\\chromedriver_win32_Latest");
+        WebDriver chromeDriver = new ChromeDriver();
+        chromeDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+        chromeDriver.get("http://www.google.com");
+        chromeDriver.quit();
     }
 }
