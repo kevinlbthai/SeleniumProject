5bc78940db87368bcb53559b12286e06b59acd22
Kevin Lim <kevinlbthai79@gmail.com>
2024-07-16 15:06:24 +0800

    commit all project files created.

--- src/main/java/HelloKevin.java
+++ src/main/java/HelloKevin.java
@@ -1,2 +1,5 @@
-package PACKAGE_NAME;public class HelloKevin {
+public class HelloKevin {
+    public static void main(String[] args) {
+        System.out.println("Hello Kevin, welcome to Selenium Project Self Learning.");
+    }
 }
