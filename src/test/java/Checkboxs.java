3b8b022a795529b9f647c843011e36e4c59409c3
Kevin Lim <kevinlbthai79@gmail.com>
2024-07-24 20:33:10 +0800

    Add new test Java class for Checkboxs test web automation. - For Chapter 4 from Selenium ESS Tutorial.

---
+++ src/test/java/Checkboxs.java
@@ -0,0 +1,32 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class CheckBoxs {
+
+    @Test
+    @DisplayName("Test Checkboxs")
+    public void checkBoxs() throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://formy-project.herokuapp.com/checkbox");
+
+        List<WebElement> checkboxList = driver.findElements(By.cssSelector("input[type='checkbox']"));
+
+        // loop through WebElement of checkboxes and select each of them
+        int size = checkboxList.size();
+        for(int i=0; i<size; i++) {
+            checkboxList.get(i).click();
+            System.out.println("Checkbox " + (i+1));
+            Thread.sleep(1000);
+        }
+
+        driver.quit();
+    }
+}
