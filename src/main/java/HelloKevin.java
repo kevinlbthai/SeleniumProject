14380346600e70e15859ba7831a6b8d6ec817a78
Kevin Lim <kevinlbthai79@gmail.com>
2024-07-24 22:42:53 +0800

    update pom.xml

--- src/main/java/HelloKevin.java
+++ src/main/java/HelloKevin.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
 
+
 public class HelloKevin {
     public static void main(String[] args) {
         System.out.println("Hello Kevin, welcome to Selenium Project Self Learning.");
